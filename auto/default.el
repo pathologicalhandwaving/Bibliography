b21211e4d54852778447c9c887b23c827ef6f857
pathologicalhandwaving <kristimshort@icloud.com>
2022-02-02 18:02:13 -0500

    default.bib

--- auto/default.el
+++ auto/default.el
@@ -2,9 +2,19 @@
  "default"
  (lambda ()
    (LaTeX-add-bibitems
+    "Hauter2021"
+    "Vasquez2002"
     "Weil1979"
+    "Voelkel2015"
+    "Ram2013"
+    "OSullivan2009"
+    "Loh2007"
+    "Swierstra2014"
     "Deagan2017"
     "Roberts2011"
-    "Dwork2019"))
+    "Dwork2019"
+    "Taleb2007"
+    "Taleb2012"
+    "Taleb2005"))
  :bibtex)
 
